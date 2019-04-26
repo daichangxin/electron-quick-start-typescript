28a445c110240114a413d5f30db4136028fb7398
daichangxin <daichangxin@gmail.com>
2019-04-26 10:05:09 +0800

    add 基础模块支持

--- src/Main.ts
+++ src/Main.ts
@@ -1,1 +1,10 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
+
 console.log(`Hello, PawBox`);
+
+async function writeHello() {
+    const txt = `${path.join(__dirname, 'hello.txt')}`;
+    await fs.writeFile(txt, 'hello, world');
+}
+writeHello();
